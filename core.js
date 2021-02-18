5fa1bc3eacad1dcb7f8b1c4ed0e3511ad47d0329
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2021-02-18 14:22:28 -0500

    Note that EGP methods are not implemented

--- core.js
+++ core.js
@@ -2061,29 +2061,6 @@ class SnmpLinuxLib
         });
   }
 
-  /*
-   * Information about a particular current UDP listener.
-   */
-  async getUdpEntry()
-  {
-  }
-
-  /*
-   * The local IP address for this UDP listener. In the case of a UDP listener
-   * which is willing to accept datagrams for any IP interface associated with
-   * the node, the value 0.0.0.0 is used.
-   */
-  async getUdpLocalAddress()
-  {
-  }
-
-  /*
-   * The local port number for this UDP listener.
-   */
-  async getUdpLocalPort()
-  {
-  }
-
 
   /*
    * *********************************************************************
@@ -2092,182 +2069,32 @@ class SnmpLinuxLib
    * Implementation of the EGP group is mandatory for all systems which
    * implement the EGP.
    * **********************************************************************
-   */
-
-  /*
-   * The number of EGP messages received without error.
-   */
-  async getEgpInMsgs()
-  {
-  }
-
-  /*
-   * The number of EGP messages received that proved to be in error.
-   */
-  async getEgpInErrors()
-  {
-  }
-
-  /*
-   * The total number of locally generated EGP messages.
-   */
-  async getEgpOutMsgs()
-  {
-  }
-
-  /*
-   * The number of locally generated EGP messages not sent due to resource
-   * limitations within an EGP entity.
-   */
-  async getEgpOutErrors()
-  {
-  }
-
-  /*
-   * The EGP neighbor table.
-   */
-  async getEgpNeighTable()
-  {
-  }
-
-  /*
-   * Information about this entity's relationship with a particular EGP
-   * neighbor.
-   */
-  async getEgpNeighEntry()
-  {
-  }
-
-  /*
-   * The EGP state of the local system with respect to this entry's EGP
-   * neighbor. Each EGP state is represented by a value that is one greater
-   * than the numerical value associated with said state in RFC 904.
-   */
-  async getEgpNeighState()
-  {
-  }
-
-  /*
-   * The IP address of this entry's EGP neighbor.
-   */
-  async getEgpNeighAddr()
-  {
-  }
-
-  /*
-   * The autonomous system of this EGP peer. Zero should be specified if the
-   * autonomous system number of the neighbor is not yet known.
-   */
-  async getEgpNeighAs()
-  {
-  }
-
-  /*
-   * The number of EGP messages received without error from this EGP peer.
-   */
-  async getEgpNeighInMsgs()
-  {
-  }
-
-  /*
-   * The number of EGP messages received from this EGP peer that proved to be
-   * in error (e.g., bad EGP checksum).
-   */
-  async getEgpNeighInErrs()
-  {
-  }
-
-  /*
-   * The number of locally generated EGP messages to this EGP peer.
-   */
-  async getEgpNeighOutMsgs()
-  {
-  }
-
-  /*
-   * The number of locally generated EGP messages not sent to this EGP peer
-   * due to resource limitations within an EGP entity.
-   */
-  async getEgpNeighOutErrs()
-  {
-  }
-
-  /*
-   * The number of EGP-defined error messages received from this EGP peer.
-   */
-  async getEgpNeighInErrMsgs()
-  {
-  }
-
-  /*
-   * The number of EGP-defined error messages sent to this EGP peer.
-   */
-  async getEgpNeighOutErrMsgs()
-  {
-  }
-
-  /*
-   * The number of EGP state transitions to the UP state with this EGP peer.
-   */
-  async getEgpNeighStateUps()
-  {
-  }
-
-  /*
-   * The number of EGP state transitions from the UP state to any other state
-   * with this EGP peer.
-   */
-  async getEgpNeighStateDowns()
-  {
-  }
-
-  /*
-   * The interval between EGP Hello command retransmissions (in hundredths of
-   * a second). This represents the t1 timer as defined in RFC 904.
-   */
-  async getEgpNeighIntervalHello()
-  {
-  }
-
-  /*
-   * The interval between EGP poll command retransmissions (in hundredths of a
-   * second). This represents the t3 timer as defined in RFC 904.
-   */
-  async getEgpNeighIntervalPoll()
-  {
-  }
-
-  /*
-   * The polling mode of this EGP entity, either passive or active.
-   */
-  async getEgpNeighMode()
-  {
-  }
-
-  /*
-   * A control variable used to trigger operator- initiated Start and Stop
-   * events. When read, this variable always returns the most recent value
-   * that egpNeighEventTrigger was set to. If it has not been set since the
-   * last initialization of the network management subsystem on the node, it
-   * returns a value of `stop'.
    *
-   * When set, this variable causes a Start or Stop event on the specified
-   * neighbor, as specified on pages 8-10 of RFC 904. Briefly, a Start event
-   * causes an Idle peer to begin neighbor acquisition and a non-Idle peer to
-   * reinitiate neighbor acquisition. A stop event causes a non-Idle peer to
-   * return to the Idle state until a Start event occurs, either via
-   * egpNeighEventTrigger or otherwise.
+   * THIS GROUP IS NOT IMPLEMENTED
+   *
+   *   getEgpInMsgs()
+   *   getEgpInErrors()
+   *   getEgpOutMsgs()
+   *   getEgpOutErrors()
+   *   getEgpNeighTable()
+   *   getEgpNeighEntry()
+   *   getEgpNeighState()
+   *   getEgpNeighAddr()
+   *   getEgpNeighAs()
+   *   getEgpNeighInMsgs()
+   *   getEgpNeighInErrs()
+   *   getEgpNeighOutMsgs()
+   *   getEgpNeighOutErrs()
+   *   getEgpNeighInErrMsgs()
+   *   getEgpNeighOutErrMsgs()
+   *   getEgpNeighStateUps()
+   *   getEgpNeighStateDowns()
+   *   getEgpNeighIntervalHello()
+   *   getEgpNeighIntervalPoll()
+   *   getEgpNeighMode()
+   *   getEgpNeighEventTrigger()
+   *   getEgpAs()
    */
-  async getEgpNeighEventTrigger()
-  {
-  }
-
-  /*
-   * The autonomous system number of this EGP entity.
-   */
-  async getEgpAs()
-  {
-  }
 }
 
 /* Flag bits for the `flags` field returned in `getIpRouteTable` entries */
